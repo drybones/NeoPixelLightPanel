9016eb666912f1b157afa7cbe0f3d4567bfa824f
David Bick <david@drybones.co.uk>
2017-01-27 20:42:56 +0000

    Local config

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // Simple red/blue fade with Node and opc.js
 
 var OPC = new require('./opc')
-var client = new OPC('raspberrypi.local', 7890);
+var client = new OPC('localhost', 7890);
 var express = require('express');
 var app = express();
 
